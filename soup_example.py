d5e39def32d6c01cab152802780b38aefa539fc7
Sergiy Chumachenko <chumachenko.sergiy@gmail.com>
2019-02-16 23:09:03 +0200

    [ADD] Primitive Group Example has added

---
+++ soup_example.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup
+import requests
+from collections import Counter
+from celery_app import app
+
+
+@app.task
+def map(url):
+    c = Counter()
+    request = requests.get(url)
+    soup = BeautifulSoup(request.text, "html.parser")
+    for word in soup.get_text().split():
+        if word not in c:
+            c[word] = 0
+        c[word] += 1
+    return c
+
+
+@app.task
+def reduce(counters):
+    res = counters[0]
+    for c in counters[1:]:
+        res += c
+    return res
